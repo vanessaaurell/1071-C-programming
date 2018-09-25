a45dbab545c6012c21588f31f31dbdbefaea6258
vanessaaurell <43335109+vanessaaurell@users.noreply.github.com>
2018-09-25 16:13:08 +0800

    add w03 file -- secondswhile.cpp

---
+++ w03/secondswhile.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	int sec;
+	int hh,mm,ss;
+	printf("Enter seconds:");
+	scanf("%d",&sec);
+	while(sec>=0){
+		hh=sec/3600;
+		mm=sec%3600/60;
+		ss=sec%60;
+	printf("%d sec = %02d:%02d:%02d\n",sec,hh,mm,ss);
+	printf("\nEnter seconds:");
+	scanf("%d",&sec);
+	}
+	
+}
