816a517d928b009dc9f2f4b53e1132c737a947b2
vanessaaurell <43335109+vanessaaurell@users.noreply.github.com>
2018-10-02 15:47:37 +0800

    add w04 file -- menu.cpp

---
+++ w04/menu.cpp
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int choice;
+    int height;
+    int width;
+    int type;
+
+    while(1)
+    {
+        printf("Main Menu\n");
+        printf("1. Rectangle\n");
+        printf("2. Vertical Triangle\n");
+        printf("3. Exit\n");
+        printf("=> ");
+        scanf("%d", &choice);
+        if(choice==3)break;
+        switch(choice)
+        {
+        case 1:
+            printf("Enter height and width: ");
+            scanf("%d %d", &height, &width);
+            for(int i=1;i<=height;i++){
+                for (int j=1;j<=width;j++){
+                if(i==1 || i==height || j==1 || j==width)
+                    printf("*");
+                else
+                    printf(" ");
+            }
+            printf("\n");
+        }
+            break;
+        case 2:
+            printf("Enter height and type: ");
+            scanf("%d %d", &height, &type);
+            switch(type)
+            {
+            case 1:
+                for(int i=1;i<=height;++i){
+                for (int j=1;j<=i;++j){
+                    if(j==1 || j==i || j==type)
+                    printf("*");
+                else if (i==height)
+                    printf("*");
+                else
+                    printf(" ");
+            }
+            printf("\n");
+        }
+                break;
+            case 2:
+            for(int i=1;i<=height;++i){
+                for (int j=i;j<=height;++j){
+                    if(i==height || j==i || j==height)
+                    printf("*");
+                else if (i==1)
+                    printf("*");
+                else
+                    printf(" ");
+            }
+            printf("\n");
+        }
+
+
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+
+            }
+
+        }
+
+    }
+
+
+    return 0;
+
+}
