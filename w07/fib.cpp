82cce74fc516debce74aeda186eda7e1d80e2138
vanessaaurell <43335109+vanessaaurell@users.noreply.github.com>
2018-10-23 15:53:11 +0800

    Create fib.cpp

---
+++ w07/fib.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+long fib(int n)
+{
+    if(n==0 || n==1) return n;
+    else
+        return fib(n-1)+fib(n-2);
+}
+
+int main()
+{
+    printf("fib(3) = %ld\n",fib(3));
+    printf("fib(9) = %ld\n",fib(9));
+    printf("fib(30) = %ld\n",fib(30));
+
+}
